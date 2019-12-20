db75cadfcb2c533b6cf6a64d470c2c6648ea4a6a
Jesus Rabelo Perez <rabeloj34@gmail.com>
2019-12-20 08:48:26 -0600

    add fleet_repair_managment

---
+++ fleet_repair_management/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
+
+from . import models
+from . import wizard
+from . import controllers
+from . import report
+#from . import report
